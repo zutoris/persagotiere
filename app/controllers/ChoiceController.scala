dbd70c5b61e1b779c9481ec92f333c09cd3ef186
carl <zutoris1975@yahoo.fr>
2016-12-04 23:18:37 +0100

    create project

---
+++ app/controllers/ChoiceController.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class ChoiceController @Inject() extends Controller {
+
+  def choice = Action {
+    // interroger la BDD
+    // si email exite, choix déjà fait
+    Ok(views.html.viewChoice("Voici ce que vous avez sélectionné :"))
+    // sinon
+    Ok(views.html.makeChoice("Faites un choix."))
+  }
+
+}
---
+++ test/schema.sql
@@ -0,0 +1,2 @@
+create table user (email varchar(50) primary key, activity number);
+create table activity(id number primary key, date date ,beginTime time,endTime time ,maxPlaces number, description varchar);
